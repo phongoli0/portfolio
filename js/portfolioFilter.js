8f83fafc35cde306b5c88c2007219f4896281cd9
phongoli0 <46616916+phongoli0@users.noreply.github.com>
2025-09-02 10:47:30 -0700

    updated default category
    updated default category

--- js/portfolioFilter.js
+++ js/portfolioFilter.js
@@ -1,19 +1,36 @@
 $(document).ready(function() {
-  // Hide all portfolio items on page load
+  // Set the default filter to ".techTsunami"
+  var defaultFilter = '.techTsunami';
+
+  // Hide all portfolio items initially
   $('.arlo_tm_portfolio_list > li').hide();
 
-  // Show items when a filter is clicked
+  // Show the items for the default filter
+  $(defaultFilter).show();
+
+  // Add the "active" class to the default filter link
+  $('.arlo_tm_portfolio_filter a[data-filter="' + defaultFilter + '"]').addClass('active');
+
+  // Handle filter clicks
   $('.arlo_tm_portfolio_filter a').on('click', function(e) {
     e.preventDefault();
+
+    // Remove "active" class from all filter links
+    $('.arlo_tm_portfolio_filter a').removeClass('active');
+
+    // Add "active" class to the clicked filter link
+    $(this).addClass('active');
+
     var filter = $(this).data('filter');
+
+    // Hide all portfolio items
     $('.arlo_tm_portfolio_list > li').hide();
+
+    // Show items based on the selected filter
     if (filter === '*') {
       $('.arlo_tm_portfolio_list > li').show();
     } else {
-      $('.arlo_tm_portfolio_list > li' + filter).show();
+      $(filter).show();
     }
-    // Optional: highlight selected filter
-    $('.arlo_tm_portfolio_filter a').removeClass('active');
-    $(this).addClass('active');
   });
 });
